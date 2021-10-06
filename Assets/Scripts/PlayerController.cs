bf136add2976b48274c9d595889a658b7946b82e
Shawn Dean <generalsmoke99@gmail.com>
2021-10-06 16:50:34 -0500

    Modifying Scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,22 +20,38 @@ public class PlayerController : MonoBehaviour
     public string VertAxis = "Vertical";
     public string FireAxis = "Fire1";
     public float MaxSpeed = 5f; //Speed
+    private bool isGrounded;
+    public float jumpForce = 10f;
+    public Vector3 jumpUp;
+
 
     private Rigidbody ThisBody = null; //Var for the ship's rigidbody.
 
     // Awake is called before start
-    private void Awake()
+    void Awake()
     {
         ThisBody = GetComponent<Rigidbody>(); //Sets the object's rigidbody to a variable.
+        jumpUp = new Vector3(0.0f, jumpForce, 0.0f);
     }//end Awake()
 
-    private void FixedUpdate()
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true; 
+    }
+    void FixedUpdate()
     {
         float Horz = Input.GetAxis(HorzAxis);
         float Vert = Input.GetAxis(VertAxis);
 
-        Vector3 MoveDirection = new Vector3(Horz, 0.0f, 0.0f);
-        transform.position += MoveDirection * MaxSpeed;
+        Vector3 MoveLeftRight = new Vector3(Horz, 0.0f, 0.0f);
+        transform.position += MoveLeftRight * MaxSpeed;
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            ThisBody.AddForce(jumpUp * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
         /*ThisBody.AddForce(MoveDirection.normalized * MaxSpeed);
 
         ThisBody.velocity = new Vector3(Mathf.Clamp(ThisBody.velocity.x, -MaxSpeed, MaxSpeed),
